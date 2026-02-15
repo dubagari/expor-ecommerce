fc8fc08a222d01ee1a0c7afdee859bffd96d89f0
Abubakar Abdullahi Dubagari <superango93@gmail.com>
2026-02-15 17:57:48 +0100

    deployment config added

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,11 +1,23 @@
 import express from 'express'
-
+import config from '../config/env.js'  
+import path from 'path'
 const app = express()
+
+const __dirname = path.resolve()
 
 app.get('/api', (req, res) => {
     res.status(200).json({message: 'success'})
 })
 
-app.listen(5000, () => {
-    console.log("port running at 5000!");
+//make app ready for deployment
+
+if (config.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../admin/dist")))
+    
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"))
+    })
+}
+app.listen(config.PORT, () => {
+    console.log(`port running at ${config.PORT}`);
 })
