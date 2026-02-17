6a4273f9f86a8c417ba6fc2dcbe1226b971bd10a
Abubakar Abdullahi Dubagari <superango93@gmail.com>
2026-02-17 07:04:40 +0100

    DB-connection

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import config from '../config/env.js'  
+import { clerkMiddleware } from '@clerk/express'
 import path from 'path'
+import connectDB from '../config/db.js'
 const app = express()
 
 const __dirname = path.resolve()
+
+app.use(clerkMiddleware())
 
 app.get('/api', (req, res) => {
     res.status(200).json({message: 'success'})
@@ -19,5 +23,6 @@ if (config.NODE_ENV === "production") {
     })
 }
 app.listen(config.PORT, () => {
-    console.log(`port running at ${config.PORT}`);
+    connectDB()
+    console.log(`server is running at port ${config.PORT}`);
 })
--- backend/config/env.js
+++ backend/config/env.js
@@ -7,6 +7,13 @@ const config = {
     MONGODB_URI: process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     NODE_ENV: process.env.NODE_ENV,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    CLERK_FRONTEND_API_KEY: process.env.CLERK_FRONTEND_API_KEY,
+    CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET,
+    INGEST_SIGNING_KEY: process.env.INGEST_SIGNING_KEY,
+    COULDNARY_API_KEY: process.env.COULDNARY_API_KEY,
+    COULDNARY_API_SECRET: process.env.COULDNARY_API_SECRET,
+    COULDNARY_CLOUD_NAME: process.env.COULDNARY_CLOUD_NAME,
 };   
 
 export default config;  
